91745a7a8ac9f3ed01081bf18768ba6227be7c79
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-06-21 09:36:43 +0530

    Update main.tf

--- gcp-libvirt-vm/gcp-main-vm/main.tf
+++ gcp-libvirt-vm/gcp-main-vm/main.tf
@@ -1,1 +1,55 @@
 
+resource "google_compute_instance" "terraform-development" {
+  boot_disk {
+    auto_delete = true
+    device_name = "terraform-development"
+
+    initialize_params {
+      image = "projects/${var.project_id}/global/images/ubuntu-22-04-nested-vm-image"
+      size  = 100
+      type  = "pd-ssd"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "n2-custom-18-71680"
+
+  metadata = {
+    enable-oslogin = "true"
+  }
+
+  name = "terraform-development"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/${var.project_id}/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "us-central1-a"
+}
