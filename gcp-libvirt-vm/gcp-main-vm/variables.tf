b7ab46738db28a7e60fde428e4f9b31ec15b5fb7
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-06-21 09:54:34 +0530

    Update variables.tf

--- gcp-libvirt-vm/gcp-main-vm/variables.tf
+++ gcp-libvirt-vm/gcp-main-vm/variables.tf
@@ -2,9 +2,3 @@ variable "project_id" {
 type =string
 }
 
-
-variable "machine_type" {
-  description = "GCP VM instance machine type."
-  type        = string
-  default     = "n2-custom-10-71680"
-}
